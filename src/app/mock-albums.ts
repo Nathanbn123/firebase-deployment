cbac847696e29c1e0158bf56e14e511bf333eb8b
Philip Eskins and Travis Toal <travis@>
2019-04-02 11:49:09 -0700

    add database functionality

---
+++ src/app/mock-albums.ts
@@ -0,0 +1,10 @@
+import { Album } from './album.model';
+
+export const ALBUMS: Album[] = [
+  new Album("Roar", "Tee Wrecks", "A live  album by the English progressive rock band originally released in 1995, on the label EMI in the United Kingdom.", 1),
+  new Album("Screechin' Flight", "Terro Dacktil", "The second  album from the American rock band, released in 1970 by Elektra Records.", 2),
+  new Album("Clever Girl", "Lossy Wrapter", "Seventh album by the Swedish band, released in 2008, based on Thor's battle with the serpent Jörmungandr.", 3),
+  new Album("Loooooooooong", "Bronte Soris", "Her highest-selling and most acclaimed album, released in 1996.", 4),
+  new Album("Horns", "Sarah Tops", "Released in 2010, this is Engerer's own rendition of the classical composer Chopin.", 5),
+  new Album("Spikes: an experience", "Steggo Soris", "Second studio album by the English-American band, released in 1967.", 6)
+];
